bea0ca3a85f3e5314d7832c2d896f20f7436d6c4
Kat Reinhart <kat.reinhart@gmail.com>
2016-07-10 19:07:37 -0700

    changed images, reconfigured sections on index.html

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,4 @@
-$.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.min.js',function(){
+$.getScript('/fullcalendar.js',function(){
   //
   // var date = new Date();
   // var d = date.getDate();
@@ -10,29 +10,10 @@ $.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.m
   $(document).ready(function(){
     $('#calendar').fullCalendar({
       googleCalendarApiKey: 'AIzaSyC-QKi9HTtEN8RwouBUA69nBnkeOqQAAHQ',
-      events: [{
-          title: 'Womens Road Race Clinic',
-          start: '2016-02-20',
-          class: 'road-event'
-      },{
-          title: 'Sequim #1',
-          start: '2016-03-12',
-          class: 'road-event'
-      },{
-          title: 'Icebreaker TT',
-          start: '2016-03-05',
-          class: 'road-event'
-      },{
-          title: 'Mason Lake #1',
-          start: '2016-03-06',
-          class: 'road-event'
-      },{
-          title: 'Dash Point',
-          start: '2016-02-21',
-          class: 'mtb-event'
-      },{
+      events: {
           googleCalendarId: '2fh1nujh8k4b49m0j2mc85lbog@group.calendar.google.com'
-      }]
+      }
+      
     });
   });
 });
---
+++ js/events.js
@@ -0,0 +1,21 @@
+events = [{
+    title: 'Womens Road Race Clinic',
+    start: '2016-02-20',
+    class: 'road-event'
+},{
+    title: 'Sequim #1',
+    start: '2016-03-12',
+    class: 'road-event'
+},{
+    title: 'Icebreaker TT',
+    start: '2016-03-05',
+    class: 'road-event'
+},{
+    title: 'Mason Lake #1',
+    start: '2016-03-06',
+    class: 'road-event'
+},{
+    title: 'Dash Point',
+    start: '2016-02-21',
+    class: 'mtb-event'
+}]

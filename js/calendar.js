c59183cac558d3a9a2293bf8698994a62d982259
Kat Reinhart <kat.reinhart@gmail.com>
2016-07-19 21:08:20 -0700

    Reconfigure calendar header and begin work on eventClick function

--- js/calendar.js
+++ js/calendar.js
@@ -1,19 +1,97 @@
-$.getScript('/fullcalendar.js',function(){
-  //
-  // var date = new Date();
-  // var d = date.getDate();
-  // var m = date.getMonth();
-  // var y = date.getFullYear();
 
-  // API KEY: AIzaSyC-QKi9HTtEN8RwouBUA69nBnkeOqQAAHQ
 
-  $(document).ready(function(){
-    $('#calendar').fullCalendar({
-      googleCalendarApiKey: 'AIzaSyC-QKi9HTtEN8RwouBUA69nBnkeOqQAAHQ',
-      events: {
-          googleCalendarId: '2fh1nujh8k4b49m0j2mc85lbog@group.calendar.google.com'
-      }
-      
-    });
+$(document).ready(function(){
+  $('#calendar').fullCalendar({
+    // FullCalendar initializeation function, pulls events from google calendar sources
+     googleCalendarApiKey: 'AIzaSyDfXGKfDOOkUPi4A6Kkp_blfRHYUNw3nts',
+     // would be nice to figure out how to use variable names for colors here, but for now hex codes will do
+     eventSources:[
+     {
+         googleCalendarId: 'en.usa#holiday@group.v.calendar.google.com',
+         className: 'holiday',
+         color: '#666'
+     },{
+         googleCalendarId: 'g7cfpme5n3h77psrsk4vqtbj4k@group.calendar.google.com',
+         className: 'event-cx',
+         color: '#C7561E'
+     },{
+         googleCalendarId: '3vi9ehsvibkfb1m31o0mqjbgec@group.calendar.google.com',
+         className: 'event-other',
+         color: '#CF9911'
+     },{
+         googleCalendarId: 'sio3juast9s4segv90hnf5om6g@group.calendar.google.com',
+         className: 'event-road',
+         color: '#AD2D2D'
+     }, {
+         googleCalendarId: '9uug907sfhgcrcjg220qlod3dg@group.calendar.google.com',
+         className: 'event-track',
+         color: '#536CA6;'
+     }, {
+         googleCalendarId: 'ovnk6hqp5kq9bh02pub51uhu0c@group.calendar.google.com',
+         className: 'event-dh',
+         color: '#9643A5'
+     }, {
+         googleCalendarId: 'nm65gk954gu676ujcbqf3hshe8@group.calendar.google.com',
+         className: 'event-enduro',
+         color: '#A7B828'
+     }, {
+         googleCalendarId: 'naf2khsbbn3artfoekqldbtfpg@group.calendar.google.com',
+         className: 'event-xco',
+         color: '#3C995B'
+     }
+
+   ],
+
+  eventClick: function(event) {
+    // Open event url in new window
+    // Currently opens link to google calendar event page
+
+    // Goal: have this link open modal window with all info, provide link to event home page etc
+    if (event.url) {
+        window.open(event.url);
+        return false;
+    }
+  },
+  // This is snippit from stackoverflow http://stackoverflow.com/questions/29072645/fullcalendar-open-bootstrap-modal-on-dayclick
+  // dayClick: function(date, jsEvent, view) {
+  //   $("#myModal").modal("show");
+  //  Still need to code the modal window. 
+  // },
+
   });
+
+// EVentually this should get DRYed out
+  $('#btn-road').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-road').toggle();
+  });
+  $('#btn-xco').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-xco').toggle();
+  });
+  $('#btn-cx').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-cx').toggle();
+  });
+  $('#btn-other').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-other').toggle();
+  });
+  $('#btn-track').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-track').toggle();
+  });
+  $('#btn-dh').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-dh').toggle();
+  });
+  $('#btn-enduro').click(function(){
+    // toggle calendar view for road calendar
+    $('.event-enduro').toggle();
+  });
+  $('#btn-hol').click(function(){
+    // toggle calendar view for road calendar
+    $('.holiday').toggle();
+  });
+
 });

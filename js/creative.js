7306ad8530367e1cc3f17c7a086c85b26a39d49a
Kat Reinhart <kat.reinhart@gmail.com>
2016-07-25 19:26:42 -0700

    Add newracerinfo page, update links in navbar

--- js/creative.js
+++ js/creative.js
@@ -8,13 +8,13 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    // $('a.page-scroll').bind('click', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    //     }, 1250, 'easeInOutExpo');
+    //     event.preventDefault();
+    // });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
@@ -38,7 +38,7 @@
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
-            top: 50
+            top: -50
         }
     })
 
